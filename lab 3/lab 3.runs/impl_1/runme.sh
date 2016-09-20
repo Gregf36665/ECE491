45ff3649954eba5f06cd654ff9b060369164f09b
birrur <birrur>
2016-09-19 23:47:21 -0400

    Added Lab 3 Repoort, Annotated Waveforms.

--- lab 3/lab 3.runs/impl_1/runme.sh
+++ lab 3/lab 3.runs/impl_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/flynng/Desktop/ECE491/lab 3/lab 3.runs/impl_1'
+HD_PWD='C:/Users/birrur/ECE491/lab 3/lab 3.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
