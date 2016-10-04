a7fb0949245dea9d3b378e5831f5038aaeaeeca1
flynng <flynng>
2016-10-04 17:04:18 -0400

    Updated fifo/ correlator

--- 4lab/4lab.sim/sim_1/behav/elaborate.bat
+++ 4lab/4lab.sim/sim_1/behav/elaborate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xelab  -wto 4c15be1f5f6b4d908c899586820b7bff -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot rx_testbench_behav xil_defaultlib.rx_testbench xil_defaultlib.glbl -log elaborate.log
+call %xv_path%/xelab  -wto 4c15be1f5f6b4d908c899586820b7bff -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot tx_rx_testbench_behav xil_defaultlib.tx_rx_testbench xil_defaultlib.glbl -log elaborate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
