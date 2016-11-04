ba2ee96840a4718a4c241dbec275725d28589ff2
flynng <flynng>
2016-11-04 11:39:58 -0400

    Updated the receiver to have a P controller

--- 5lab/5lab.sim/sim_1/behav/xsim.dir/mx_rcvr_test_behav/webtalk/xsim_webtalk.tcl
+++ 5lab/5lab.sim/sim_1/behav/xsim.dir/mx_rcvr_test_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/flynng/Desktop/ECE491/5lab/5lab.sim/sim_1/behav/xsim.dir/mx_rcvr_test_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Nov 01 12:35:42 2016" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Fri Nov 04 11:38:31 2016" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2016.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "1577090" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "34.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "13711575 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "13751575 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "5.04_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "17096_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3989644496 -regid "" -xml C:/Users/flynng/Desktop/ECE491/5lab/5lab.sim/sim_1/behav/xsim.dir/mx_rcvr_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/flynng/Desktop/ECE491/5lab/5lab.sim/sim_1/behav/xsim.dir/mx_rcvr_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/flynng/Desktop/ECE491/5lab/5lab.sim/sim_1/behav/xsim.dir/mx_rcvr_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "5.54_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "17116_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2241754938 -regid "" -xml C:/Users/flynng/Desktop/ECE491/5lab/5lab.sim/sim_1/behav/xsim.dir/mx_rcvr_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/flynng/Desktop/ECE491/5lab/5lab.sim/sim_1/behav/xsim.dir/mx_rcvr_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/flynng/Desktop/ECE491/5lab/5lab.sim/sim_1/behav/xsim.dir/mx_rcvr_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- 5lab/5lab.srcs/sources_1/new/FSMs.sv
+++ 5lab/5lab.srcs/sources_1/new/FSMs.sv
@@ -33,9 +33,12 @@ module FSMs(input logic clk, reset, preamble_match, sfd_match, match_error,
 	logic enable_data;
 	logic data_done;
 	logic write_next;
+	logic inc_error, dec_error;
+	logic [5:0] error_change; // How far out of sync are we
 
 	fsm_pll  U_PLL    (.clk, .reset, .data_bit(data_bit_last), .enable_pll, .sample_count, 
-			           .current_corr(zero_one_strength), .sample_inc, .sample_dec);
+			           .current_corr(zero_one_strength), .sample_inc(inc_error), .sample_dec(dec_error),
+					   .error_change);
 
 	fsm_psfd U_DETECT (.clk, .reset, .preamble_match, .sfd_match, .set_ferr,
 					   .data_done,  .slow_sample_count, .cardet, .bit_count_reset,
@@ -51,4 +54,7 @@ module FSMs(input logic clk, reset, preamble_match, sfd_match, match_error,
 	one_bit_ff U_LAST_BIT (.clk, .reset, .enb(store_bit), .D(data_bit), .Q(data_bit_last));
 					
 	sync_input U_WRITE (.clk, .async_in(write_next), .sync_out(write));
+
+	accumulator U_ERROR_ACCUM (.clk, .reset, .inc_error, .dec_error, 
+								.error_change, .sample_inc, .sample_dec);
 endmodule
